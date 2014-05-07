8228fc1a592298092ae68ecb21e2c3e40f7d0acc
Jhoan Garcia <jhoanegar@gmail.com>
2014-05-07 02:05:36 -0500

    Add yambaclient library

--- src/examples/yamba/StatusActivity.java
+++ src/examples/yamba/StatusActivity.java
@@ -1,6 +1,7 @@
 package examples.yamba;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -9,6 +10,9 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.marakana.android.yamba.clientlib.*;
 
 public class StatusActivity extends Activity implements OnClickListener {
 	private final static String TAG = "StatusActivity";
@@ -30,5 +34,29 @@ public class StatusActivity extends Activity implements OnClickListener {
 		String status = editStatus.getText().toString();
 		Log.d(TAG,"onClick with status: " + status);
 		
+		new PostTask().execute(status);
+	}
+	
+	private final class PostTask extends AsyncTask<String, Void, String> {
+
+		@Override
+		protected String doInBackground(String... params) {
+			YambaClient yambaCloud = new YambaClient("student","password");
+			try{
+				yambaCloud.postStatus(params[0]);
+				return "Successfully posted";
+				
+			} catch(YambaClientException e){
+				e.printStackTrace();
+				return "Failed to post to yamba service";
+			}
+		}
+		
+		@Override 
+		protected void onPostExecute(String result){
+			super.onPostExecute(result);
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+		}
+	
 	}
 }
