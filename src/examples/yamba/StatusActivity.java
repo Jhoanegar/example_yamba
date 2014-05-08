ae2c2edf3306cbdec291f8f025d982758fabec8e
Jhoan Garcia <jhoanegar@gmail.com>
2014-05-08 02:23:01 -0500

    Add textWatcher and textCounter

--- src/examples/yamba/StatusActivity.java
+++ src/examples/yamba/StatusActivity.java
@@ -1,17 +1,23 @@
 package examples.yamba;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class StatusActivity extends Activity implements OnClickListener {
 	private final static String TAG = "StatusActivity";
 	private EditText editStatus;
 	private Button buttonTweet;
+	private TextView textCount;
+	private int defaultTextColor;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,48 @@ public class StatusActivity extends Activity implements OnClickListener {
 		editStatus = (EditText) findViewById(R.id.editStatus);
 		buttonTweet = (Button) findViewById(R.id.buttonTweet);
 		buttonTweet.setOnClickListener(this);
+		textCount = (TextView) findViewById(R.id.textCount);
+		defaultTextColor = textCount.getTextColors().getDefaultColor();
+		
+		editStatus.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				int charsLeft = 140 - editStatus.length();
+				textCount.setText(Integer.toString(charsLeft));
+				textCount.setTextColor(getColorBasedOnCharsLeft(charsLeft));
+			}
+			
+			private int getColorBasedOnCharsLeft(int charsLeft){
+				int green = Color.rgb(49, 158, 12);
+				int yellow = Color.rgb(157, 150, 12);
+				int red = Color.rgb(157, 20, 12);
+				
+				int color = green;
+				
+				if (charsLeft < 10) {
+					color = yellow;
+				}
+				if (charsLeft < 0) {
+					color = red;
+				}
+				
+				return color;
+			}
+		});
 	}
 
 	@Override
