271add124a01fd12b03e607134f6331b37810d73
Jhoan Garcia <jhoanegar@gmail.com>
2014-05-08 03:25:37 -0500

    Dynamically load the fragment into the view

--- src/examples/yamba/StatusActivity.java
+++ src/examples/yamba/StatusActivity.java
@@ -5,12 +5,20 @@ import android.os.Bundle;
 
 public class StatusActivity extends Activity {
 
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.new_activity_status);
-	}
 
+		// Check if this activity was created before
+		if (savedInstanceState == null) {
+			// Create fragment
+			StatusFragment fragment = new StatusFragment();
+			getFragmentManager()
+					.beginTransaction()
+					.add(android.R.id.content, fragment,
+							fragment.getClass().getSimpleName()).commit();
+			
+		}
+	}
 
 }
