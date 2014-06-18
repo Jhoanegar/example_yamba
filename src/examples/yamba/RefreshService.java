8da228a4f4ffc6bfff304f6c1bd1da67ea0b1340
Jhoan Garcia <jhoanegar@gmail.com>
2014-06-18 04:40:09 -0500

    Pull timelines from yamba

--- src/examples/yamba/RefreshService.java
+++ src/examples/yamba/RefreshService.java
@@ -1,9 +1,19 @@
 package examples.yamba;
 
+import java.util.List;
+
+import com.marakana.android.yamba.clientlib.YambaClient;
+import com.marakana.android.yamba.clientlib.YambaClient.Status;
+import com.marakana.android.yamba.clientlib.YambaClientException;
+
 import android.app.IntentService;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
 import android.util.Log;
+import android.widget.Toast;
 
 public class RefreshService extends IntentService {
 	static final String TAG = "RefreshService";
@@ -25,7 +35,24 @@ public class RefreshService extends IntentService {
 
 	@Override
 	public void onHandleIntent(Intent intent) {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		final String username = prefs.getString("username","");
+		final String password = prefs.getString("password","");
+		if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)){
+			Toast.makeText(this, "Update your username and password", Toast.LENGTH_LONG).show();
+		}
 		Log.d(TAG,"onStart");
+		
+		YambaClient cloud = new YambaClient(username,password);
+		try{
+			List<Status> timeline = cloud.getTimeline(20);
+			for (Status status : timeline){
+				Log.d(TAG,String.format("%s: %s", status.getUser(),status.getMessage()));
+			}
+		} catch (YambaClientException e){
+			Log.e(TAG, "Failed to fetch timeline",e);
+			e.printStackTrace();
+		}
 		return;
 	}
 
