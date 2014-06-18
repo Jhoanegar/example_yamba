5c63e2ef8542db83a1dcec2ba92b6393d981182a
Jhoan Garcia <jhoanegar@gmail.com>
2014-06-18 03:35:27 -0500

    Add service shell

---
+++ src/examples/yamba/RefreshService.java
@@ -0,0 +1,40 @@
+package examples.yamba;
+
+import android.app.IntentService;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class RefreshService extends IntentService {
+	static final String TAG = "RefreshService";
+	
+	public RefreshService(){
+		super(TAG);
+	}
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Log.d(TAG,"onCreate");
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG,"onDestroy");
+	}
+
+	@Override
+	public void onHandleIntent(Intent intent) {
+		Log.d(TAG,"onStart");
+		return;
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
