7729af51635d449df36285e9a271c3edc22a1de5
Jhoan <jhoanegar@gmail.com>
2014-06-21 21:33:28 -0500

    Remove deprecated constructor

--- src/examples/yamba/TimelineFragment.java
+++ src/examples/yamba/TimelineFragment.java
@@ -6,7 +6,11 @@ import android.content.CursorLoader;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.View;
+import android.widget.SimpleCursorAdapter.ViewBinder;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.text.format.DateUtils;
 import android.util.Log;
 
 public class TimelineFragment extends ListFragment implements
@@ -22,14 +26,15 @@ public class TimelineFragment extends ListFragment implements
 
 	private SimpleCursorAdapter myAdapter;
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		myAdapter = new SimpleCursorAdapter(getActivity(), R.layout.list_item,
-				null, FROM, TO);
+				null, FROM, TO, 0);
+
+		myAdapter.setViewBinder(new TimelineViewBinder());
 		setListAdapter(myAdapter);
-		
+
 		getLoaderManager().initLoader(LOADER_ID, null, this);
 	}
 
@@ -56,4 +61,22 @@ public class TimelineFragment extends ListFragment implements
 		myAdapter.swapCursor(null);
 
 	}
+	static final class TimelineViewBinder implements ViewBinder {
+
+		@Override
+		public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+			if (view.getId() != R.id.list_item_text_created_at) {
+				return false;
+			}
+
+			long timestamp = cursor.getLong(columnIndex);
+			CharSequence relativeTime = DateUtils
+					.getRelativeTimeSpanString(timestamp);
+			((TextView) view).setText(relativeTime);
+
+			return true;
+		}
+	}
 }
+
+
